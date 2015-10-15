bc96b477854e2090652d0683d5f2f29cf9e578e7
padilla <padilla@uji.es>
2015-10-15 14:48:20 +0200

    Esqueleto inicial del ConnectionFactory de la UJI.

--- es.uji.control.domain.ujioracle/src/es/uji/control/domain/ujioracle/internal/UjiOracleComponent.java
+++ es.uji.control.domain.ujioracle/src/es/uji/control/domain/ujioracle/internal/UjiOracleComponent.java
@@ -1,5 +1,9 @@
 package es.uji.control.domain.ujioracle.internal;
 
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -9,17 +13,23 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.prefs.PreferencesService;
 
+import es.uji.control.domain.service.connectionfactory.ControlConnectionException;
+import es.uji.control.domain.service.connectionfactory.IControlConnection;
+import es.uji.control.domain.spi.IControlConnectionFactorySPI;
 import es.uji.control.domain.ujioracle.UjiOracleConnectionFactoryConfig;
 
 @Component(name="factory.ujioracle", immediate=true)
 public class UjiOracleComponent implements UjiOracleConnectionFactoryConfig {
+	
+	static final String CONNECTION_FACTORY_KEY_VALUE = "UJIORA";
+	static final String CONNECTION_FACTORY_DESCRIPTION_VALUE = "Backend UJI/Oracle via JDBC";
 
+	private BundleContext bundlecontext;
 	private PreferencesService preferencesService;
 
 	@Activate
 	public void activate(ComponentContext componentContext) throws Exception {
-		synchronized (this) {
-		}
+		this.bundlecontext = componentContext.getBundleContext();
 	}
 
 	@Deactivate
@@ -47,6 +57,45 @@ public class UjiOracleComponent implements UjiOracleConnectionFactoryConfig {
 	@Override
 	public String getURL() {
 		return null;
+	}
+	
+	private ServiceRegistration<IControlConnectionFactorySPI> registration;
+	private ConnectionFactorySPIImpl impl;
+	
+	private void registerConnectionFactory() {
+		if (registration != null) {
+			throw new IllegalStateException("El servicio ya esta registrado.");
+		} else {
+			Hashtable<String, Object> properties = new Hashtable<>();
+			properties.put(IControlConnectionFactorySPI.CONNECTION_FACTORY_KEY, CONNECTION_FACTORY_KEY_VALUE);
+			properties.put(IControlConnectionFactorySPI.CONNECTION_FACTORY_DESCRIPTION, CONNECTION_FACTORY_DESCRIPTION_VALUE);
+			impl = new ConnectionFactorySPIImpl();
+			registration = bundlecontext.registerService(IControlConnectionFactorySPI.class, impl, properties);
+		}
+		
+	}
+	
+	private void unregisterConnectionFactory() {
+		if (registration != null) {
+			registration.unregister();
+			registration = null;
+			impl.reset();
+			impl = null;
+		}
+	}
+	
+	private class ConnectionFactorySPIImpl implements IControlConnectionFactorySPI {
+
+		@Override
+		public IControlConnection createConnection() throws ControlConnectionException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		public void reset() {
+			
+		}
+		
 	}
 
 }
